cc0bccb0c666c28358be4f9d9d10fe60be39576a
bnj997 <bnj997@gmail.com>
2020-08-16 19:59:12 +1000

    Made FIFO Method

---
+++ src/Stocks/Nab.js
@@ -0,0 +1,216 @@
+import React, {useState} from 'react';
+import MaterialTable from "material-table";
+import { Button } from '@material-ui/core';
+
+const Nab = () => {
+  const [state, setState] = useState({
+    columns: [
+      { 
+        title: 'Date', 
+        field: 'date' },
+      { 
+        title: 'Bought', 
+        field: 'bought' },
+      { 
+        title: 'Unit Price', 
+        field: 'unitPrice', 
+        type: 'numeric' },
+      {
+        title: 'Brockerage',
+        field: 'brockerage',
+      },
+      {
+        title: 'Total Cost',
+        field: 'totalCost',
+      },
+    ],
+    data: [
+      { 
+        date: '16/03/2020', 
+        bought: 60, 
+        unitPrice: 16.68, 
+        brockerage: 10.00,
+        totalCost: 1010.8
+      },
+      {
+        date: '16/03/2020', 
+        bought: 58, 
+        unitPrice: 17.10, 
+        brockerage: 10.00,
+        totalCost: 1001.8
+      },
+      {
+        date: '16/03/2020', 
+        bought: 58, 
+        unitPrice: 17.01, 
+        brockerage: 10.00,
+        totalCost: 996.58
+      },
+      {
+        date: '27/03/2020', 
+        bought: -85, 
+        unitPrice: 15.78, 
+        brockerage: 19.95,
+        totalCost: -1321.41
+      },
+      {
+        date: '8/04/2020', 
+        bought: 64, 
+        unitPrice: 15.50, 
+        brockerage: 10.00,
+        totalCost: 1002.0
+      },
+      {
+        date: '12/04/2020', 
+        bought: 64, 
+        unitPrice: 15.48, 
+        brockerage: 10.00,
+        totalCost: 1000.72
+      },
+    ],
+    data: [
+      { 
+        date: '6/01/2020', 
+        bought: 10, 
+        unitPrice: 235.00, 
+        brockerage: 19.95,
+        totalCost: 2369.95,
+        costBase: 237.00
+      },
+      {
+        date: '23/03/2020', 
+        bought: 14, 
+        unitPrice: 195.00, 
+        brockerage: 19.95,
+        totalCost: 2749.95,
+        costBase: 196.43
+      },
+      {
+        date: '27/03/2020', 
+        bought: -8, 
+        unitPrice: 216.00, 
+        brockerage: 19.95,
+        totalCost: -1708.05,
+        costBase: 213.51
+      },
+      {
+        date: '7/04/2020', 
+        bought: -8, 
+        unitPrice: 216.47, 
+        brockerage: 19.95,
+        totalCost: -1711.81,
+        costBase: 213.98
+      }
+    ],
+    // data: [
+    //   { 
+    //     date: '6/01/2020', 
+    //     bought: 2, 
+    //     unitPrice: 235.00, 
+    //     brockerage: 10.00,
+    //     totalCost: 480.00
+    //   },
+    //   {
+    //     date: '23/03/2020', 
+    //     bought: 14, 
+    //     unitPrice: 195.00, 
+    //     brockerage: 19.95,
+    //     totalCost: 2749.95
+    //   },
+    //   {
+    //     date: '7/04/2020', 
+    //     bought: -8, 
+    //     unitPrice: 216.47, 
+    //     brockerage: 19.95,
+    //     totalCost: -1711.81
+    //   }
+    // ],
+  });
+
+
+  const calculateCapGain = (table) => {
+    var transArray = [];
+    var capGainArray = [];
+    var dateSold;
+    var batch;
+    var costBase;
+    var batchProp;
+    var costProp;
+    var salesValue;
+    var capGain;
+
+    //iterate through main table
+    for (var i = 0; i <= table.length - 1; i++) {
+      transArray.push(table[i])
+      if (transArray[i].bought < 0) {
+        var numStocksSold = -(transArray[i].bought)
+        for (var j = 0; j <= transArray.length - 1; j++) {
+          if (numStocksSold > 0) {
+            if (numStocksSold > transArray[j].bought ) {
+              numStocksSold = numStocksSold - transArray[j].bought; //different
+              dateSold = transArray[j].date; //same
+              batch = transArray[j].bought; //same
+              costBase = transArray[j].costBase; //same
+              batchProp = transArray[j].bought; //different
+              costProp = batchProp * costBase; //same
+              salesValue = batchProp * (transArray[i].totalCost / transArray[i].bought); //same
+              capGain = salesValue - costProp; //same
+              capGainArray.push(
+                {
+                  dateSold: dateSold,
+                  batch: batch,
+                  costBase: costBase,
+                  batchProp: batchProp,
+                  costProp: costProp, 
+                  salesValue: salesValue,
+                  capGain: capGain,
+                }
+              )
+              transArray[j].bought = 0
+            } 
+            else {
+              dateSold = transArray[j].date;
+              batch = transArray[j].bought;
+              costBase = transArray[j].costBase;
+              batchProp = numStocksSold;
+              costProp = batchProp * costBase;
+              salesValue = batchProp * (transArray[i].totalCost / transArray[i].bought);
+              capGain = salesValue - costProp;
+              capGainArray.push(
+                {
+                  dateSold: dateSold,
+                  batch: batch,
+                  costBase: costBase,
+                  batchProp: batchProp,
+                  costProp: costProp, 
+                  salesValue: salesValue,
+                  capGain: capGain,
+                }
+              )
+              transArray[j].bought = transArray[j].bought - numStocksSold
+              numStocksSold = 0
+            }
+          } else {
+            break;
+          }
+        }
+      }
+    }
+    console.log(transArray);
+    console.log(capGainArray);
+  };
+
+
+  return (
+    <React.Fragment>
+      <MaterialTable style={{width: "80%"}}
+        title="NAB"
+        columns={state.columns}
+        data={state.data}
+      />
+      <Button onClick={() => calculateCapGain(state.data)}> CALCULATE </Button>
+    </React.Fragment>
+  )
+}
+
+export default Nab;
