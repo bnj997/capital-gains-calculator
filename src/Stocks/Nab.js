97ab8fa0351b1449fac7cd79543cccf0f23a5e26
bnj997 <bnj997@gmail.com>
2020-08-16 20:31:54 +1000

    Made cost base calculations automatic to prevent rounding errors

--- src/Stocks/Nab.js
+++ src/Stocks/Nab.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import MaterialTable from "material-table";
-import { Button } from '@material-ui/core';
+import { Button, TableContainer } from '@material-ui/core';
 
 const Nab = () => {
   const [state, setState] = useState({
@@ -24,113 +24,80 @@ const Nab = () => {
         field: 'totalCost',
       },
     ],
-    data: [
-      { 
-        date: '16/03/2020', 
-        bought: 60, 
-        unitPrice: 16.68, 
-        brockerage: 10.00,
-        totalCost: 1010.8,
-        costBase: 16.85
-      },
-      {
-        date: '16/03/2020', 
-        bought: 58, 
-        unitPrice: 17.10, 
-        brockerage: 10.00,
-        totalCost: 1001.8,
-        costBase: 17.27
-      },
-      {
-        date: '16/03/2020', 
-        bought: 58, 
-        unitPrice: 17.01, 
-        brockerage: 10.00,
-        totalCost: 996.58,
-        costBase: 17.18
-      },
-      {
-        date: '27/03/2020', 
-        bought: -85, 
-        unitPrice: 15.78, 
-        brockerage: 19.95,
-        totalCost: -1321.41,
-        costBase: 15.55
-      },
-      {
-        date: '8/04/2020', 
-        bought: 64, 
-        unitPrice: 15.50, 
-        brockerage: 10.00,
-        totalCost: 1002.0,
-        costBase: 15.66
-      },
-      {
-        date: '12/04/2020', 
-        bought: 64, 
-        unitPrice: 15.48, 
-        brockerage: 10.00,
-        totalCost: 1000.72,
-        costBase: 15.64
-      },
-    ],
     // data: [
     //   { 
-    //     date: '6/01/2020', 
-    //     bought: 10, 
-    //     unitPrice: 235.00, 
-    //     brockerage: 19.95,
-    //     totalCost: 2369.95,
-    //     costBase: 237.00
+    //     date: '16/03/2020', 
+    //     bought: 60, 
+    //     unitPrice: 16.68, 
+    //     brockerage: 10.00,
+    //     totalCost: 1010.8,
     //   },
     //   {
-    //     date: '23/03/2020', 
-    //     bought: 14, 
-    //     unitPrice: 195.00, 
-    //     brockerage: 19.95,
-    //     totalCost: 2749.95,
-    //     costBase: 196.43
+    //     date: '16/03/2020', 
+    //     bought: 58, 
+    //     unitPrice: 17.10, 
+    //     brockerage: 10.00,
+    //     totalCost: 1001.8,
+    //   },
+    //   {
+    //     date: '16/03/2020', 
+    //     bought: 58, 
+    //     unitPrice: 17.01, 
+    //     brockerage: 10.00,
+    //     totalCost: 996.58,
     //   },
     //   {
     //     date: '27/03/2020', 
-    //     bought: -8, 
-    //     unitPrice: 216.00, 
+    //     bought: -85, 
+    //     unitPrice: 15.78, 
     //     brockerage: 19.95,
-    //     totalCost: -1708.05,
-    //     costBase: 213.51
+    //     totalCost: -1321.41,
     //   },
     //   {
-    //     date: '7/04/2020', 
-    //     bought: -8, 
-    //     unitPrice: 216.47, 
-    //     brockerage: 19.95,
-    //     totalCost: -1711.81,
-    //     costBase: 213.98
-    //   }
-    // ],
-    // data: [
-    //   { 
-    //     date: '6/01/2020', 
-    //     bought: 2, 
-    //     unitPrice: 235.00, 
+    //     date: '8/04/2020', 
+    //     bought: 64, 
+    //     unitPrice: 15.50, 
     //     brockerage: 10.00,
-    //     totalCost: 480.00
+    //     totalCost: 1002.0,
     //   },
     //   {
-    //     date: '23/03/2020', 
-    //     bought: 14, 
-    //     unitPrice: 195.00, 
-    //     brockerage: 19.95,
-    //     totalCost: 2749.95
+    //     date: '12/04/2020', 
+    //     bought: 64, 
+    //     unitPrice: 15.48, 
+    //     brockerage: 10.00,
+    //     totalCost: 1000.72,
     //   },
-    //   {
-    //     date: '7/04/2020', 
-    //     bought: -8, 
-    //     unitPrice: 216.47, 
-    //     brockerage: 19.95,
-    //     totalCost: -1711.81
-    //   }
     // ],
+    data: [
+      { 
+        date: '6/01/2020', 
+        bought: 10, 
+        unitPrice: 235.00, 
+        brockerage: 19.95,
+        totalCost: 2369.95,
+      },
+      {
+        date: '23/03/2020', 
+        bought: 14, 
+        unitPrice: 195.00, 
+        brockerage: 19.95,
+        totalCost: 2749.95,
+      },
+      {
+        date: '27/03/2020', 
+        bought: -8, 
+        unitPrice: 216.00, 
+        brockerage: 19.95,
+        totalCost: -1708.05,
+      },
+      {
+        date: '7/04/2020', 
+        bought: -8, 
+        unitPrice: 216.47, 
+        brockerage: 19.95,
+        totalCost: -1711.81,
+      }
+    ],
   });
 
 
@@ -146,6 +113,10 @@ const Nab = () => {
     var capGain;
 
     for (var i = 0; i <= table.length - 1; i++) {
+      //calculate costbase
+      var costBase = table[i].totalCost / table[i].bought
+      table[i].costBase = costBase
+
       transArray.push(table[i])
       if (transArray[i].bought < 0) {
         var numStocksSold = -(transArray[i].bought)
